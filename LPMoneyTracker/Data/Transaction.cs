891c72a1296ce62646ea88bfb3f20d1bebe9c470
RosenbergLiu <xygenio@hotmail.com>
2023-07-07 18:54:17 +1000

    ini

---
+++ LPMoneyTracker/Data/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LPMoneyTracker.Data
+{
+    public class Transaction
+    {
+        [Key]
+        public Guid TransId { get; set; }
+        [Required]
+        public DateTime TransDate { get; set; }
+        [Required]
+        public string Merchant { get; set; }
+        [Required]
+        public double Amount { get; set; }
+        public string Category { get; set; }
+        public string Note { get; set; }
+        public string Account { get; set; }
+
+        [Required]
+        public bool IsSubscription { get; set; } = false;
+
+        [Required]
+        public bool IsAccident { get; set; } = false;
+    }
+}
